a711920a13a2665fd3d5e21490afd338ab27a328
Сергей <porenti.tv@gmail.com>
2022-04-02 22:18:53 +0500

    update Женя
    добавил ввод ключа пользователя

--- 4/main.py
+++ 4/main.py
@@ -6,6 +6,9 @@ window = tk.Tk()
 entry_input = tk.Entry(
 width=25)
 entry_input.pack()
+modul_input = tk.Entry(
+width=25)
+modul_input.pack()
 entry_output = tk.Entry(
 width=25)
 entry_output.pack()
@@ -13,6 +16,7 @@ entry_output.pack()
 def push():
     entry_output.delete(0)
     text = entry_input.get()
+    _mod = modul_input.get() #нельзя больше количества букв
 
     _text = []
     for item in text:
@@ -26,11 +30,13 @@ def push():
 
     _modul_list = []
     for i in range(_len): #считаем значение по модулю
-        _modul_list.append((_text.index(text[i])+1+_random_list[i])%_len)
+        _modul_list.append((_text.index(text[i])+1+_random_list[i])%int(_mod))
 
     _output_text = ''
     for i in range(_len): #шифруем со значениями по модулю
         _output_text += text[_modul_list[i]]
+
+    print(_modul_list, _random_list, _text)
 
 
     entry_output.insert(0, _output_text)
